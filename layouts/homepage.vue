c8592912e27d542912e0f55df9b5f9a007e682ef
Binyu <binyu1231@gmail.com>
2025-05-21 16:12:10 +0800

    chore: update

--- layouts/homepage.vue
+++ layouts/homepage.vue
@@ -8,10 +8,22 @@ defineOptions({ name: 'homepage' })
     <TheSection>
       <div class="md:grow md:pl-64 lg:pr-6 xl:pr-0">
         <div class="pt-16 md:pt-20 pb-8 md:pl-6 lg:pl-12">
-          hi~
-          <NuxtLink to="/language/javascript/ecmascript-features">
-            doc
-          </NuxtLink>
+          <h1>收拾精神，自作主宰</h1>
+          
+          <div>
+            <NuxtLink to="/language/javascript/ecmascript-features">
+              <h4>Knowledge</h4>
+            </NuxtLink>
+            <NuxtLink to="/live/food/drink">
+              <h4>Live</h4>
+            </NuxtLink>
+            <NuxtLink to="/language/javascript/ecmascript-features">
+              <h4>Language</h4>
+            </NuxtLink>
+            <NuxtLink to="/language/javascript/ecmascript-features">
+              <h4>Framework</h4>
+            </NuxtLink>
+          </div>
         </div>
       </div>
     </TheSection>
